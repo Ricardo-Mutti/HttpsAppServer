bdb58f12b12a848970f674a3e0f658e30ee37032
Mutti <mutti.appsimples@gmail.com>
2017-03-29 11:15:23 -0300

    Add server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const express = require('express')
 const router = express.Router()
 const app = express()
 const directoryToServe = 'client'
-const port = 3443
+const portHttps = 3443
+const portHttp = 3442
 
 app.use(bodyparser())
 app.use('/api', router)
@@ -16,14 +17,26 @@ app.use('/', express.static(path.join(__dirname, '..', directoryToServe)))
 
 router.get('/', (req, res) => {
   res.json({
-    hello: 'world'
+    success: true,
+    message: "Get request done right!",
+    response:
+    {
+      attribute1:"Attribute1",
+      attribute2:"Attribute2"
+   }
   })
 })
 
 // POST
 router.post('/', (req, res) => {
   res.json({
-    body: req.body.body
+    success: true,
+    message: "Post request done right!",
+    response:
+    {
+      attribute1:req.body.attribute1,
+      attribute2:req.body.attribute2
+   }
   })
 })
 
@@ -37,6 +50,10 @@ router.post('/', (req, res) => {
 //     console.log(`Serving the ${directoryToServe}/ directory at https://localhost:${port}`)
 //   })
 
-http.createServer(app).listen(port, function () {
-    console.log(`Server listening at ${port}`)
+http.createServer(app).listen(portHttps, function () {
+    console.log(`Https server listening at ${portHttps}`)
+  })
+
+http.createServer(app).listen(portHttp, function () {
+    console.log(`Http server listening at ${portHttp}`)
   })
